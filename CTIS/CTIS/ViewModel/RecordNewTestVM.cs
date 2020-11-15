cd2f84af82bccf7a022580715291ea5fa0550147
Derra <herppiederp@gmail.com>
2020-11-15 08:08:12 +0800

    Completed RecordNewTest its VM

---
+++ CTIS/CTIS/ViewModel/RecordNewTestVM.cs
@@ -0,0 +1,123 @@
+﻿using CTIS.Model;
+using CTIS.Utilities;
+using CTIS.View;
+using System;
+using System.Collections.ObjectModel;
+using System.Net.Mail;
+using Xamarin.Forms;
+
+namespace CTIS.ViewModel
+{
+    public class RecordNewTestVM : BaseVM
+    {
+        public CovidTest CovidTest { get; set; }
+        public ObservableCollection<string> StatusList { get; set; }
+        public ObservableCollection<string> ResultList { get; set; }
+        public Command CancelCommand { get; set; }
+        public Command SaveCommand { get; set; }
+        public Command BackCommand { get; set; }
+
+        public RecordNewTestVM()
+        {
+            CovidTest = new CovidTest();
+            StatusList = new ObservableCollection<string>();
+            ResultList = new ObservableCollection<string>();
+            StatusList.Add("Pending result");
+            StatusList.Add("Test completed");
+            StatusList.Add("Under review");
+            ResultList.Add("Positive");
+            ResultList.Add("Negative");
+            Status = "Pending result";
+            TestedBy = App.User.Name;
+            SaveCommand = new Command(SaveTestExecute);
+            BackCommand = new Command(BackExecute);
+            CancelCommand = new Command(CancelExecute);
+        }
+
+        private async void BackExecute(Object obj)
+        {
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private async void CancelExecute(Object obj)
+        {
+            await Application.Current.MainPage.DisplayAlert("Cancel Confirmation", "Your input will not be saved. Are you sure?", "Yes", "No");
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private async void SaveTestExecute(Object obj)
+        {
+            if (string.IsNullOrWhiteSpace(CovidTest.PatientName))
+            {
+                await Application.Current.MainPage.DisplayAlert("Missing field", "Patient name cannot be empty.", "OK");
+                return;
+            }
+            else
+            {
+                await FirebaseDBConnection.AddTestAsync(CovidTest);
+                await Application.Current.MainPage.Navigation.PopAsync();
+            }
+            
+        }
+
+        public string PatientName
+        {
+            get { return CovidTest.PatientName; }
+            set
+            {
+                CovidTest.PatientName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime TestDate
+        {
+            get { return CovidTest.TestDate; }
+            set
+            {
+                CovidTest.TestDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Result
+        {
+            get { return CovidTest.Result; }
+            set
+            {
+                CovidTest.Result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime ResultDate
+        {
+            get { return CovidTest.ResultDate; }
+            set
+            {
+                CovidTest.ResultDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Status
+        {
+            get { return CovidTest.Status; }
+            set
+            {
+                CovidTest.Status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TestedBy
+        {
+            get { return CovidTest.TestedBy; }
+            set
+            {
+                CovidTest.TestedBy = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
