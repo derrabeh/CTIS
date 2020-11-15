8e604f34a088263a171f1da3d6876d0abe199473
Derra <herppiederp@gmail.com>
2020-11-15 08:04:57 +0800

    Added Custom Font (Goldman+Arimo) for use in logo, etc.

--- CTIS/CTIS/AssemblyInfo.cs
+++ CTIS/CTIS/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+//Custom font for CTIS Logo
+[assembly: ExportFont("Goldman-Regular.ttf", Alias = "Goldman")]
+[assembly: ExportFont("Arimo.ttf", Alias = "Arimo")]
