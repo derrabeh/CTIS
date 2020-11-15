1c7ae080e97cc903cd9c110159832bcf091fb67b
Derra <herppiederp@gmail.com>
2020-11-15 08:07:31 +0800

    Added TestCentre and TestKit class

---
+++ CTIS/CTIS/Model/TestCentre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTIS.Model
+{
+    public class TestCentre
+    {
+        public string CentreID { get; set; }
+        public string CentreName { get; set; }
+            
+    }
+}
---
+++ CTIS/CTIS/Model/TestKit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTIS.Model
+{
+    public class TestKit
+    {
+        public string KitID { get; set; }
+        public string KitName { get; set; }
+        public string CurrentStock { get; set; } //available stock 
+    }
+}
