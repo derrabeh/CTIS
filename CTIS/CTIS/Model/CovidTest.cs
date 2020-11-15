e7dce2cbe195bf7e20214a01cb4d146d70176ad8
Derra <herppiederp@gmail.com>
2020-11-15 08:07:15 +0800

    Modified User and CovidTest class

--- CTIS/CTIS/Model/CovidTest.cs
+++ CTIS/CTIS/Model/CovidTest.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace CTIS.Model
 {
-    class CovidTest
+    public class CovidTest
     {
+        public string TestID { get; set; }
+        public string PatientName { get; set; } //how to link to a patient instance...? 
+        public DateTime TestDate { get; set; }
+        public string Result { get; set; } //Positive, Negative 
+        public DateTime ResultDate { get; set; }
+        public string Status { get; set; } //Pending, Complete, Under review
+        public string TestedBy { get; set; }
+        //test.TestedBy = App.User.Name;  
+        //^ logs the name of tester who recorded this test 
     }
 }
--- CTIS/CTIS/Model/User.cs
+++ CTIS/CTIS/Model/User.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace CTIS.Model
 {
-    class User
+    public class User
     {
+        public string UserID { get; set; } //auto-generated
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Passport { get; set; }
+        public string Type { get; set; }
+        //if user is initialized from Register page, user type is automatically Patient
+        //new center officers must be registered by another existing officer 
 
+        //Patient types: returnee, quarantined, close contact, infected, suspected
+        //Officer positions: tester, manager 
     }
 }
